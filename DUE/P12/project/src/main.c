fd86e695318c8e1b35ad88c162a2895a457434e7
Jose Leitao <jose.p.leitao@gmail.com>
2018-11-18 11:12:02 -0200

    Criação do P12 no repositório AS7, utilizando plug-in GIT no AS7

---
+++ DUE/P12/project/src/main.c
@@ -0,0 +1,21 @@
+/**
+ * \file
+ *
+ * \Projeto 12
+ *
+ */
+
+
+
+
+#include <asf.h>
+
+int main (void)
+{
+	/* Insert system clock initialization code here (sysclk_init()). */
+
+	board_init();
+
+	/* Insert application code here, after the board has been initialized. */
+	return 0;
+}
